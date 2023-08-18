78ee15e29d3cf3219fccb5dfe0d4450d6fe7a3bd
minsik321 <alstlr832110gmail.com>
2023-08-18 16:27:56 +0900

    chore: Number에 대한 네이밍 방식을 통일

--- Lotto.java
+++ Lotto.java
@@ -3,40 +3,35 @@ import java.util.List;
 
 public class Lotto {
     public static void main(String[] args) {
-        List<Integer> lottos = new ArrayList<>();
-
+        List<Integer> winningLottoNumbers = new ArrayList<>();
         final int[] myLottoNumbers = {1,2,3,4,5,6};
-
-        getWinningNumberOfLotto(lottos);
-
-        confirmOfMatchNumber(myLottoNumbers,lottos);
+        getWinningLottoNumbers(winningLottoNumbers);
+        confirmMatchLottoNumbers(myLottoNumbers,winningLottoNumbers);
     }
 
-    public static void getWinningNumberOfLotto (List<Integer> lottos){
-        while (lottos.size() < 6) {
+    public static void getWinningLottoNumbers(List<Integer> winningLottoNumbers){
+        while (winningLottoNumbers.size() < 6) {
             int randomNumber = (int)(Math.random() * 45) + 1;
-            if (!lottos.contains(randomNumber))
-                lottos.add(randomNumber);
+            if (!winningLottoNumbers.contains(randomNumber))
+                winningLottoNumbers.add(randomNumber);
         }
-        System.out.println("로또 당첨 번호는 " + lottos + " 입니다.\n");
+        System.out.println("로또 당첨 번호는 " + winningLottoNumbers + " 입니다.\n");
     }
 
-    public static void confirmOfMatchNumber(int[] arrays, List<Integer> list) {
-        List<Integer> matchNumbers = new ArrayList<>();
-
+    public static void confirmMatchLottoNumbers(int[] arrays, List<Integer> list) {
+        List<Integer> matchLottoNumbers = new ArrayList<>();
         for (int array : arrays) {
             if (list.contains(array)) {
-                matchNumbers.add(array);
+                matchLottoNumbers.add(array);
             }
         }
-
-        if (matchNumbers.isEmpty()) {
+        if (matchLottoNumbers.isEmpty()) {
             System.out.println("아쉽지만 겹치는 번호가 없습니다.");
         } else {
             System.out.print("축하합니다! 겹치는 번호는 ");
-            for (int matchNumber = 0; matchNumber < matchNumbers.size(); matchNumber++) {
-                System.out.print(matchNumbers.get(matchNumber));
-                if (matchNumber < matchNumbers.size() - 1) {
+            for (int matchNumber = 0; matchNumber < matchLottoNumbers.size(); matchNumber++) {
+                System.out.print(matchLottoNumbers.get(matchNumber));
+                if (matchNumber < matchLottoNumbers.size() - 1) {
                     System.out.print(", ");
                 }
             } System.out.println(" 입니다!");
