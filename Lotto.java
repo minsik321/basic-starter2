7c5adb48caf75ba64863e7d78ff9ea8ecd0db2f5
minsik321 <alstlr832110gmail.com>
2023-08-18 16:45:46 +0900

    feat: 출력부분 메서드 추가 및 수정

--- Lotto.java
+++ Lotto.java
@@ -9,16 +9,18 @@ public class Lotto {
         confirmMatchLottoNumbers(myLottoNumbers,winningLottoNumbers);
     }
 
-    public static void getWinningLottoNumbers(List<Integer> winningLottoNumbers){
+    static void getWinningLottoNumbers(List<Integer> winningLottoNumbers){
         while (winningLottoNumbers.size() < 6) {
             int randomNumber = (int)(Math.random() * 45) + 1;
             if (!winningLottoNumbers.contains(randomNumber))
                 winningLottoNumbers.add(randomNumber);
         }
-        System.out.println("로또 당첨 번호는 " + winningLottoNumbers + " 입니다.\n");
+        System.out.print("로또 당첨 번호는 ");
+        printLottoNumbers(winningLottoNumbers);
+        System.out.println(" 입니다.\n");
     }
 
-    public static void confirmMatchLottoNumbers(int[] arrays, List<Integer> list) {
+    static void confirmMatchLottoNumbers(int[] arrays, List<Integer> list) {
         List<Integer> matchLottoNumbers = new ArrayList<>();
         for (int array : arrays) {
             if (list.contains(array)) {
@@ -29,12 +31,18 @@ public class Lotto {
             System.out.println("아쉽지만 겹치는 번호가 없습니다.");
         } else {
             System.out.print("축하합니다! 겹치는 번호는 ");
-            for (int matchNumber = 0; matchNumber < matchLottoNumbers.size(); matchNumber++) {
-                System.out.print(matchLottoNumbers.get(matchNumber));
-                if (matchNumber < matchLottoNumbers.size() - 1) {
-                    System.out.print(", ");
-                }
-            } System.out.println(" 입니다!");
+            printLottoNumbers(matchLottoNumbers);
+            System.out.println(" 입니다!");
         }
+    }
+
+    static void printLottoNumbers(List<Integer> list) {
+        for (int lottoNumber = 0; lottoNumber < list.size(); lottoNumber++) {
+            System.out.print(list.get(lottoNumber));
+            if (lottoNumber < list.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+
     }
 }
