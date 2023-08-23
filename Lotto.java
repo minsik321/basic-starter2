0e70fa165e02f24888420e0d98b75e6855773ff5
minsik321 <alstlr832110gmail.com>
2023-08-23 19:52:55 +0900

    feat: 회차에 따른 당첨번호 저장 함수 추가

--- Lotto.java
+++ Lotto.java
@@ -1,8 +1,11 @@
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringJoiner;
 
 public class Lotto {
     public static void main(String[] args) {
+        HashMap<String, StringJoiner> roundOfWinningLottoNumbers = new HashMap<>();
     }
     static HashSet<Integer> getWinningLottoNumbers() {
         HashSet<Integer> winningLottoNumbers = new HashSet<>();
@@ -18,5 +21,19 @@ public class Lotto {
             stringJoinerTypeWinningLottoNumbers.add(String.valueOf(ordinalNumber));
         }
         return stringJoinerTypeWinningLottoNumbers;
+    }
+    static void saveWinningLottoNumbers(HashMap<String ,StringJoiner> roundOfWinningLottoNumbers) {
+        String MaxRoundOfNumber;
+        if (roundOfWinningLottoNumbers.keySet().isEmpty()) {
+            MaxRoundOfNumber = "0회차";
+        } else {
+            MaxRoundOfNumber = Collections.max(roundOfWinningLottoNumbers.keySet());
+        }
+        roundOfWinningLottoNumbers.put(Integer.parseInt(MaxRoundOfNumber.substring(0,1))+1 + "회차",changeType(getWinningLottoNumbers()));
+    }
+    static void numberOfSaveWinningLottoNumbers(int number, HashMap<String ,StringJoiner> roundOfWinningLottoNumbers) {
+        for (int ordinalNumber = 0; ordinalNumber < number; ordinalNumber++) {
+            saveWinningLottoNumbers(roundOfWinningLottoNumbers);
+        }
     }
 }
